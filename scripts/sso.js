7f187860be1ebc03fea75e096b499cebe2c57cf1
Jeff Collins <htejeff@gmail.com>
2019-11-04 00:57:48 -0800

    fff

--- scripts/sso.js
+++ scripts/sso.js
@@ -1,16 +1,16 @@
 var googleUser = {};
-	var startApp = function() {
-	gapi.load('auth2', function(){
-	  // Retrieve the singleton for the GoogleAuth library and set up the client.
-	  auth2 = gapi.auth2.init({
-		client_id: '735698212957-dvu2h24tapar68t9f8p7b66uhaamc96f.apps.googleusercontent.com',
-		cookiepolicy: 'single_host_origin',
-		// Request scopes in addition to 'profile' and 'email'
-		//scope: 'additional_scope'
-	  });
-	  // attachSignin(document.getElementById('customBtn'));
-	});
-	};
+	// var startApp = function() {
+	// gapi.load('auth2', function(){
+	//   // Retrieve the singleton for the GoogleAuth library and set up the client.
+	//   auth2 = gapi.auth2.init({
+	// 	client_id: '735698212957-dvu2h24tapar68t9f8p7b66uhaamc96f.apps.googleusercontent.com',
+	// 	cookiepolicy: 'single_host_origin',
+	// 	// Request scopes in addition to 'profile' and 'email'
+	// 	//scope: 'additional_scope'
+	//   });
+	//   // attachSignin(document.getElementById('customBtn'));
+	// });
+	// };
 
 
 	function signIn() {
@@ -27,7 +27,7 @@ var googleUser = {};
 		  // var firstName = fullName.split(" ")[0];
 		  // var lastName = fullName.split(" ")[1];
 		  // var email = profile.getEmail();
-		  document.getElementById("f_Name").innerHTML = 'State: Signed in';
+		  // document.getElementById("f_Name").innerHTML = 'State: Signed in';
 		  // document.getElementById("l_Name").innerHTML = 'Last Name: ' + lastName;
 		  // document.getElementById("u_email").innerHTML = 'email: ' + email;
 	  }
@@ -50,6 +50,6 @@ var googleUser = {};
 		console.log('User signed out.');
 		document.getElementById('customBtn').innerHTML = "Sign In"
 		document.getElementById('customBtn').onclick = signIn;
-		document.getElementById("f_Name").innerHTML = 'State: Signed out';
+		// document.getElementById("f_Name").innerHTML = 'State: Signed out';
 	});
 	}
