4c74f743cee0cdd59099f4ca6caab1b657bcfd01
Chris Nelson <christopher.nelson@usa.com>
2019-12-02 15:26:00 -0800

    update sign out

--- profile/index.php
+++ profile/index.php
@@ -159,10 +159,12 @@
 	<!-- <script type="text/javascript" src="../scripts/sso.js"></script> -->
 	<script>
 	  function signOut() {
-		var auth2 = gapi.auth2.getAuthInstance();
-		auth2.signOut().then(function () {
-		  console.log('User signed out.');
-		  window.location.href = "http://ironbronco.jrcollins.com";
+		window.location.replace("https://appengine.google.com/_ah/logout?continue=http://ironbronco.jrcollins.com)
+		// var auth2 = gapi.auth2.getAuthInstance();
+		// auth2.signOut().then(function () {
+		//   console.log('User signed out.');
+		//   window.location.href = "http://ironbronco.jrcollins.com";
+
 		});
 	  }
 	</script>
